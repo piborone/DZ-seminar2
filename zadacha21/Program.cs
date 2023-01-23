b6d0494ca786f8d2168c33eaf93fb23f7005ef8c
Pavel B <boronenkov999@mail.ru>
2023-01-23 19:36:55 +0300

    исправлена опечатка вывода точки В

--- zadacha21/Program.cs
+++ zadacha21/Program.cs
@@ -14,7 +14,7 @@ int numbersY2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("введите Z2 ");
 int numbersZ2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Координаты точки А ({numbersX1}, {numbersY1}, {numbersZ1});");
-Console.WriteLine($"Координаты точки А ({numbersX2}, {numbersY2}, {numbersZ2});");
+Console.WriteLine($"Координаты точки В ({numbersX2}, {numbersY2}, {numbersZ2});");
 double rezult = Math.Sqrt((Math.Pow(numbersX2-numbersX1,2))+(Math.Pow(numbersY2-numbersY1,2))+(Math.Pow(numbersZ2-numbersZ1,2)));
 double coord = Math.Round(rezult, 2, MidpointRounding.AwayFromZero);
 Console.WriteLine($"Расстояние между точками в 3D пространстве - {coord}");
