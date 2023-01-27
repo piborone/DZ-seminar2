ebafeffeeaeb8469e8ac3bb50c6693e8ab8408dc
Pavel B <boronenkov999@mail.ru>
2023-01-27 20:49:55 +0300

    решение задачи 27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sum(int numbers)
+
+{
+
+string? text = Convert.ToString(numbers);
+int numbersA = Convert.ToInt32(Convert.ToString(text!.Length));
+int sum = 0;
+    for(int i = 0; i < numbersA; i++)
+    {
+      sum = sum + Convert.ToInt32(Convert.ToString(text[i]));
+    }
+    return sum;
+}
+
+// Console.Write("введите число: ");
+// string? text = Console.ReadLine();
+// int numbers = Convert.ToInt32(Convert.ToString(text!.Length));
+// int sum = 0;
+// for(int i = 0; i < numbers; i++)
+// {
+//    sum = sum + Convert.ToInt32(Convert.ToString(text[i]));
+// }
+// Console.WriteLine(sum);
+
+
+
+int InputNumbers(string str)
+{
+    int numbers;
+    string text;
+    while(true)
+    {
+        Console.WriteLine(str);
+        text = Console.ReadLine();
+        if(int.TryParse(text, out numbers))
+        {
+            break;
+        }
+        Console.WriteLine("Ошибка!!! Введено не верное значение!");
+    }
+    return numbers;
+}
+int A = InputNumbers("Введите число А");
+int sum = Sum(A);
+Console.WriteLine($"сумма всех чисел цифры {A} = {sum}");
+
