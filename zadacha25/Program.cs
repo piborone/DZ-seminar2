1a08c7369c07cc4327dc8dc498481eee386232d4
Pavel B <boronenkov999@mail.ru>
2023-01-27 13:13:19 +0300

    Реализоваен вывод В

--- zadacha25/Program.cs
+++ zadacha25/Program.cs
@@ -2,12 +2,11 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int Ste(int numbers)
+int Ste(int numbers, int numbersC)
 
 {
     int ste = numbers;
-    Console.WriteLine("введите число В: ");
-    int numbersB = Convert.ToInt32(Console.ReadLine());
+    int numbersB = numbersC;
     for(int i = 1; i < numbersB; i++)
     {
         numbers = numbers*ste; 
@@ -17,6 +16,7 @@ int Ste(int numbers)
 
 Console.WriteLine("введите число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
-int steA = Ste(A);
-Console.WriteLine($"А в степени В = {steA}");
-
+Console.WriteLine("введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int steA = Ste(A, B);
+Console.WriteLine($"{A} в степени {B} = {steA}");
