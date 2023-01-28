fabf444c66eec3247cfd31304f157799aca108a9
Pavel B <boronenkov999@mail.ru>
2023-01-28 18:21:21 +0300

    решение задачи 29

--- zadacha29/Program.cs
+++ zadacha29/Program.cs
@@ -22,33 +22,29 @@
 
 int Proverka(int numbersA)
 {
-    while(true)
-    {
-                
-        if(numbersA > 0 && numbersA <= 8)
-        {
             int[] array = new int[numbersA]; 
             for(int i = 0; i < numbersA; i++)
             {
             array[i] = new Random().Next(10);    
             }
             Console.WriteLine($"[{String.Join(", ", array)}]");
-            break;
-        }
-                
-        System.Console.WriteLine("Ошибка!!!");
-        System.Console.WriteLine("Введенное число не неходится в диапозоне от 1 до 8");
-        break;
-        
-    }
+                      
     return numbersA;
 }
 
 Console.WriteLine("Программа создаст массив длинной от 1 до 8 элементов");
 Console.WriteLine("Введите длинну массива меньше или равную 8 элементам");
 int numbers = Convert.ToInt32(Console.ReadLine());
-int numbersA = Proverka(numbers);
-
+if(numbers > 0 && numbers <= 8)
+    {
+    int array = Proverka(numbers); 
+      
+    }  
+    else
+    {    
+    System.Console.WriteLine("Ошибка!!!");
+    System.Console.WriteLine("Введенное число не неходится в диапозоне от 1 до 8");
+    }
 
 
     
